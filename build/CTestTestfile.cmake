f460f736bd4edc8848f036f8e1a736c259f74081
boiling <419610242@qq.com>
2022-07-01 02:01:05 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rendu/repositories/cmake-tutorial
+# Build directory: /home/rendu/repositories/cmake-tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
