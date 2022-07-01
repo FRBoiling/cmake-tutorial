11f14c169869899865c7c5a77c76ce5a4d1c5805
Boil <FRee22858@live.com>
2022-07-01 20:08:27 +0800

    Delete 文件夹

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rendu/repositories/cmake-tutorial
-# Build directory: /home/rendu/repositories/cmake-tutorial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
